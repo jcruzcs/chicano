df2c0f8abf05db5e127bbd046e8ca405a7d07c20
jcruzcs <jesse.cruzz.cs@gmail.com>
2024-06-02 15:43:44 -0700

    grammar err

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
         </div>
       </main>
       <footer className="site-footer">
-        <p>&copy; 2024 El Chino Artist. All rights reserved.</p>
+        <p>&copy; 2024 El Chicano Artist. All rights reserved.</p>
       </footer>
     </div>
   );
