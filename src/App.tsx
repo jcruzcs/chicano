a9ab330eb673dab78f037984bc19c5f720c75864
jcruzcs <jesse.cruzz.cs@gmail.com>
2024-06-02 15:20:05 -0700

    ray

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import Header from './Header';
 import './App.css';
+import ray1 from './assets/ray1.png';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main>{/* Main content sections */}</main>
+      <main>{/* Main content sections */
+      
+      <img src={ray1} alt="ray1" className="ray1" />
+      
+      }</main>
+      
 
       <footer className="site-footer">
         <p>&copy; 2024 El Chicano Artist. All rights reserved.</p>
